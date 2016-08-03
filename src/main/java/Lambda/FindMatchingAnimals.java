0bc14b301eebd1870bd7e4af57b48bfe65f0f3e3
Yared Tseghu <jaredtse@hotmail.com>
2016-08-03 18:19:47 +0100

    Minor changes

--- src/main/java/Lambda/FindMatchingAnimals.java
+++ src/main/java/Lambda/FindMatchingAnimals.java
@@ -2,14 +2,18 @@ package Lambda;
 
 public class FindMatchingAnimals {
 
-    private static void print( Animal animal, CheckTrait checkTrait){
-        if( checkTrait.test(animal)){
+    private static void print( Animal animal, CheckTrait checkTrait ){
+
+        if( checkTrait.test( animal )){
             System.out.println( animal );
         }
+
     }
 
     public static void main(String... args){
+
         print( new Animal("Fish", false, true), a -> a.canSwim() );
         print( new Animal("kangaroo", true, false), a -> a.canHop() );
+
     }
 }
